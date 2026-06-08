6c75588cb17739061d0af489e07bc87d94979688
benzmehdi58-spec <mehdi.benzineb.0504@univ-sba.dz>
2026-06-08 10:54:14 +0100

    Update CommandPalette.tsx

--- src/app/components/CommandPalette.tsx
+++ src/app/components/CommandPalette.tsx
@@ -59,7 +59,7 @@ export function CommandPalette() {
           </CommandItem>
           <CommandItem onSelect={() => runCommand(() => navigate('/models'))}>
             <Brain className="mr-2 h-4 w-4" />
-            <span>ML Models</span>
+            <span>Models</span>
           </CommandItem>
           <CommandItem onSelect={() => runCommand(() => navigate('/settings'))}>
             <Settings className="mr-2 h-4 w-4" />
